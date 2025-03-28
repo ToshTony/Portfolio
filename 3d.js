af0adbca6a57b2a836c5e5cfa5636afcb9f77adf
Tony <user@localhost.localdomain>
2025-03-28 07:16:26 +0000

    color

--- 3d.js
+++ 3d.js
@@ -5,7 +5,8 @@ const canvas = document.getElementById('3dimage');
 
 // 1. Create the scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color('#F0F0F0');
+// scene.background = new THREE.Color('#F0F0F0');
+scene.background = new THREE.Color('#FFFFFF');
 
 // 2. Add the Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
